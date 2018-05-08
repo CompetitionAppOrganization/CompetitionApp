b691ce2475900851a7f7981977111abe3155ce22
praneetmek <praneetmek@gmail.com>
2018-05-08 08:30:33 -0400

    Firebase Functional

---
+++ app/src/main/java/com/example/andrea/competitionapp/AdminCompInfo.java
@@ -0,0 +1,13 @@
+package com.example.andrea.competitionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminCompInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_comp_info);
+    }
+}
